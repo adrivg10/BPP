f4116219924749f852985eff988757029014c0ad
adrivg <adrianvalverdegarrido@hotmail.com>
2022-01-31 15:16:23 +0100

    lección 1 hecha a través de funciones

--- lec1.py
+++ lec1.py
@@ -1,49 +1,56 @@
 import pandas as pd
+import pandas as pd
 import numpy as np
 import os
 import matplotlib.pyplot as plt
 import csv
 
 
-def ejercicio(path_archivo):
+def csv_file(csv_file):
+    df = pd.read_csv(csv_file, sep="\t")
+    return df.sum(axis=0)
 
-    df = pd.read_csv(PATH_CSV, sep="\t")
-    '''for column in df.columns:
-        df[column] = pd.to_numeric(df[column], errors='coerce')
-    df = df.replace(np.nan, 0, regex=True)
-    for column in df.columns:
-        df[column] = df[column].astype(int)'''
-    resumen = df.sum(axis=0)
-    print("xxxxxxxx Resumen mensual xxxxxxxxxxxxx")
-    print(resumen, "\n")
-    print("¿Qué mes se ha gastado más?")
-    print(np.absolute(resumen.min()))
-    print("¿Qué mes se ha ahorrado más?")
-    print(resumen.max())
-    print("¿Cuál es la media de gastos al año?")
-    print(np.absolute(resumen.mean().round(decimals=2)))
-    print("¿Cuál ha sido el gasto total a lo largo del año?")
+
+def maximo(dataframe):
+    return dataframe.max()
+
+
+def minimo(dataframe):
+    return dataframe.min()
+
+
+def media(dataframe):
+    return dataframe.mean().round(decimals=2)
+
+
+def ingresos(dataframe):
     ingresos = 0
+    for keys in dataframe.keys():
+        if dataframe[keys] > 0:
+            ingresos += dataframe[keys]
+    return ingresos
+
+
+def gastos(dataframe):
     gastos = 0
-    for keys in resumen.keys(): # Se crea un bucle para recorrer todos los meses
-        if resumen[keys] < 0:
-            gastos += resumen[keys] # si para ese mes el numero es negativo se considera gasto
-        else:
-            ingresos += resumen[keys] # si para ese mes el numero es positivo/0 se considera ingreso
-    print(np.absolute(gastos))
-    print("¿Cuáles han sido los ingresos totales a lo largo del año?")
-    print(ingresos)
-    print("Realice una gráfica de la evolución de ingresos a lo largo del año")
-    meses = [] # Creamos una lista en la que se van a ir guardando como evolucionan los ingresos a lo largo del año
+    for keys in dataframe.keys():
+        if dataframe[keys] < 0:
+            gastos += dataframe[keys]
+    return gastos
+
+
+def grafica(dataframe):
+    meses = []
     a = 0
-    for keys in resumen.keys():
-        a += resumen[keys] # Para saber el computo global vamos a ir sumando al mes anterior el propio (en el caso de que sea negativo lo restaremos)
-        meses.append(a) # Vamos añadiendo el computo global de cada mes en una lista
-    grafica = pd.DataFrame(meses, index=df.columns, columns=['Evolucion_Precios']) #Transformamos la lista en un dataframe
-    plt.bar(df.columns, grafica['Evolucion_Precios']) # Grafica tipo bar
-    plt.xlabel('Meses') # nombre eje x
-    plt.ylabel('Ingresos') # nombre eje y
-    plt.show() # Mostramos la imagen creada
+    df = pd.read_csv(PATH_CSV, sep="\t")
+    for keys in dataframe.keys():
+        a += dataframe[keys]
+        meses.append(a)
+    grafica = pd.DataFrame(meses, index=df.columns, columns=['Evolucion_Precios'])
+    plt.bar(df.columns, grafica['Evolucion_Precios'])
+    plt.xlabel('Meses')
+    plt.ylabel('Ingresos')
+    plt.show()
 
 
 MEDIA_ROOT = os.path.expanduser("~/Python/Buenas practicas de programacion/lec1/")
@@ -51,6 +58,7 @@ PATH_CSV = MEDIA_ROOT + 'finanzas2020.csv'
 fieldnames = []
 filas = []
 filas2 = []
+
 try:
     with open(PATH_CSV) as File:
         reader = csv.reader(File, delimiter='\t')
@@ -74,8 +82,20 @@ try:
                 writer.writerow(fieldnames)
                 for row in filas2:
                     writer.writerow(row)
+            print("xxxxxxxx Resumen mensual xxxxxxxxxxxxx")
+            print(csv_file(PATH_CSV), "\n")
+            print("¿Qué mes se ha gastado más?")
+            print(np.absolute(minimo(csv_file(PATH_CSV))))
+            print("¿Qué mes se ha ahorrado más?")
+            print(maximo(csv_file(PATH_CSV)))
+            print("¿Cuál es la media de gastos al año?")
+            print(np.absolute(media(csv_file(PATH_CSV))))
+            print("¿Cuál ha sido el gasto total a lo largo del año?")
+            print(np.absolute(gastos(csv_file(PATH_CSV))))
+            print("¿Cuáles han sido los ingresos totales a lo largo del año?")
+            print(ingresos(csv_file(PATH_CSV)))
 
-            ejercicio(PATH_CSV)
+            grafica(csv_file(PATH_CSV))
 
         else:
             print("El archivo no cuenta con 12 columnas")
